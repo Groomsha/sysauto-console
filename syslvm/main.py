67b750ca289a1d82f0f6efda45736cb5fc9912c1
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2023-01-06 11:23:08 +0200

    add: sudo check

--- syslvm/main.py
+++ syslvm/main.py
@@ -1,17 +1,25 @@
+import os
 import subprocess
+from elevate import elevate
 
+
+def is_root():
+        if os.getuid() != 0:
+            elevate()
 
 if __name__ == '__main__':
-    while True:
-        key = input('P - Exit: ').lower()
+    is_root()
 
-        if key == 'q':
-            break
-        elif key == 'p':
-            password = input("Please enter your password: ")
-            p = subprocess.Popen('ls'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            p.communicate(password.encode())
+    # while True:
+    #     key = input('P - Exit: ').lower()
 
-            print(p.returncode)
-            print(p.stdout)
+    #     if key == 'q':
+    #         break
+    #     elif key == 'p':
+    #         pass
+    #         # password = input("Please enter your password: ")
+            # p = subprocess.Popen('ls'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # p.communicate(password.encode())
 
+            # print(p.returncode)
+            # print(p.stdout)
