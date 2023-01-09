ee8d0b9bc6162960fb547796560266c0ec2a2003
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2023-01-09 16:23:55 +0200

    upd: create main menu

---
+++ syslvm/lvm_menu.py
@@ -0,0 +1,30 @@
+class LVM_menu:
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def main_menu() -> str:
+        return """
+        P - Physical Volume Info/Create
+        L - Logical Volume Info/Create
+        Q - Exit\n"""
+
+    @staticmethod
+    def physical_menu() -> str:
+        return """
+        I - Physical Volume Info
+        C - Physical Volume Create
+        D - Physical Volume Remove
+        R - Return
+        Q - Exit\n"""
+
+    @staticmethod
+    def logical_menu() -> str:
+        return """
+        I - Logical Volume Info
+        C - Logical Volume Create
+        U - Logical Volume Resize
+        D - Logical Volume Remove
+        R - Return
+        Q - Exit\n"""
+    
---
+++ syslvm/maincmd.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+from typing import Dict
+from elevate import elevate
+
+from lvm_menu import LVM_menu
+
+
+def is_root() -> None:
+    if os.getuid() != 0:
+        elevate(graphical=False)
+
+def cmd_command(command:str) -> Dict:
+    cmd = subprocess.run(command.split(), shell=True, encoding='utf-8', 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
+    
+    return {'returncode': cmd.returncode,
+            'stdout': cmd.stdout,
+            'stderr': cmd.stderr}
+
+
+if __name__ == '__main__':
+    cmd_menu = LVM_menu()
+    current_menu = cmd_menu.main_menu()
+
+    # is_root()
+
+    while True:
+        subprocess.run(['clear'], shell=True)
+        print('Utility for Fast Configuration of LVM v0.3')
+
+        key = input(f"{current_menu}\nPlease Select Menu: ").lower()
+
+        match key:
+            case 'p': current_menu = cmd_menu.physical_menu()
+            case 'l': current_menu = cmd_menu.logical_menu()
+            case 'r': current_menu = cmd_menu.main_menu()
+            case 'q': break 
+    
+        
